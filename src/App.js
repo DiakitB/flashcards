7045744313b13f36a01fb61a19f1055c4df1d655
vanessaMarcuse <babacar.diakte01@gmail.com>
2023-07-31 02:28:18 -0400

    testing react hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FlashCards />
     </div>
   );
 }
 
-export default App;
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+    questionN: "1"
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+    questionN: "2"
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+    questionN: "3"
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+    questionN: "4"
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+    questionN: "5"
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+    questionN: "6"
+  }
+];
+function FlashCards() {
+  const [selId, setId] = useState(null);
+  const [text, setText] = useState("");
+  function clickHandler(id) {
+    setId(id);
+  }
+  function onChangeHandler(e) {
+    e.preventDefault();
+
+    const newText = { text: text };
+    console.log(newText);
+  }
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => clickHandler(question.id)}
+          className={question.id === selId ? "selected" : {}}
+        >
+          <p>{question.id === selId ? question.answer : question.question}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
